abaf895d6dc045e6717417e7614794beee56f024
d.yuji <bronzsord@gmail.com>
2017-05-10 21:12:24 +0900

    test sample javaFX code

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,25 +1,34 @@
 package application;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Button btn = new Button();
+			btn.setText("Say 'Hello World'");
+			btn.setOnAction(new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						System.out.println("Hello World!");
+					}
+				}
+			);
+
+			StackPane root = new StackPane();
+			root.getChildren().add(btn);
+			Scene scene = new Scene(root, 300, 250);
+			primaryStage.setTitle("Hello World!");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
 	}
-
 	public static void main(String[] args) {
 		launch(args);
 	}
