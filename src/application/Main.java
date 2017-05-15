47ebc0241fb17ff51476c1b5a1e1d3c1471dfbd4
d.yuji <bronzsord@gmail.com>
2017-05-15 18:33:09 +0900

    ファイル読み込み、書き出し

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,6 +14,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,19 +22,38 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 			Button btn = new Button();
-			FileChooser file = new FileChooser();
+			FileChooser fc = new FileChooser();
+			StackPane root = new StackPane();//Pane:コンポーネントを埋め込む
+			BorderPane pane = new BorderPane();
+			fc.getExtensionFilters().addAll(
+						new ExtensionFilter("Text Files", "*.txt"),
+						new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
+						new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
+						new ExtensionFilter("All Files", "*.*"));
+
 			btn.setText("open file");
 			btn.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent event) {
 //						System.out.println("Hello World!");
-						file.showOpenDialog(primaryStage);
+						File openFile = fc.showOpenDialog(primaryStage);
+						if(openFile != null){
+							System.out.println("get file path");
+//							fc.showSaveDialog(primaryStage);
+							try {
+								Writer writer = new FileWriter(fc.showSaveDialog(primaryStage));
+								writer.write("test");
+								writer.close();
+							} catch (IOException e) {
+								// TODO 自動生成された catch ブロック
+								e.printStackTrace();
+							}
+						}
 					}
 				}
 			);
 
-			StackPane root = new StackPane();//Pane:コンポーネントを埋め込む
-			BorderPane pane = new BorderPane();
+
 			Label label = new Label("This is JavaFX!");
 			root.getChildren().add(btn);
 			pane.setCenter(label);
