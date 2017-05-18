164bafe067ec9694f37053f5a72be820472c857a
d.yuji <bronzsord@gmail.com>
2017-05-18 21:22:02 +0900

    抽象メソッドの追加

--- src/application/AbstractScene.java
+++ src/application/AbstractScene.java
@@ -1,7 +1,11 @@
 package application;
 
 import javafx.application.Application;
+import javafx.scene.layout.Pane;
 
 public abstract class AbstractScene extends Application{
+		public abstract Pane loadFXMLLayout();
+		public abstract void showLayout();
+		public abstract void moveScene();
 
 }
--- src/application/AddContentScene.java
+++ src/application/AddContentScene.java
@@ -1,11 +1,30 @@
 package application;
 
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class AddContentScene extends AbstractScene {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public Pane loadFXMLLayout() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	@Override
+	public void showLayout() {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+	@Override
+	public void moveScene() {
 		// TODO 自動生成されたメソッド・スタブ
 
 	}
