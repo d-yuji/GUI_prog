31f7768061cf2f1d9e04f244dc3b501cd698c8d6
d.yuji <bronzsord@gmail.com>
2017-05-18 21:16:47 +0900

    Sceneの具体クラスの追加

---
+++ src/application/AddContentScene.java
@@ -0,0 +1,13 @@
+package application;
+
+import javafx.stage.Stage;
+
+public class AddContentScene extends AbstractScene {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
