2f76ff1f2e8439e1f14ad45d719b652db02e8728
d.yuji <bronzsord@gmail.com>
2017-05-19 14:04:27 +0900

    ファイル読み出し、編集、書き出し

--- src/application/EditScene.java
+++ src/application/EditScene.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -8,9 +9,10 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -19,19 +21,24 @@ public class EditScene extends AbstractScene {
 	private static AbstractScene content = new ContentScene();
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		// TODO 自動生成されたメソッド・スタブ
 		Button btn = new Button();
 		Button fopen = new Button();
+		Button fsave = new Button();
+		VBox box = new VBox();
  		BorderPane pane = new BorderPane();
 		FileChooser fc = new FileChooser();
+		TextArea textarea = new TextArea();
 		fc.getExtensionFilters().addAll(
 					new ExtensionFilter("Text Files", "*.txt"),
 					new ExtensionFilter("Image Files", "*.png", "*.jpg", "*.gif"),
 					new ExtensionFilter("Audio Files", "*.wav", "*.mp3", "*.aac"),
 					new ExtensionFilter("All Files", "*.*")
-			);
+		);
 		btn.setText("move state");
 		fopen.setText("open file");
+		fsave.setText("save File");
+
+		textarea.setText("file value");
 		btn.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
@@ -48,16 +55,30 @@ public class EditScene extends AbstractScene {
 			@Override
 			public void handle(ActionEvent event) {
 					try {
-							fileOpen(fc,primaryStage,pane);
+							fileOpen(fc,primaryStage,pane,textarea);
 					} catch (Exception e) {
 						// TODO 自動生成された catch ブロック
 						e.printStackTrace();
 					}
 				}
 		});
-		pane.setCenter(new Label("edit"));
+		fsave.setOnAction(new EventHandler<ActionEvent>(){
+			@Override
+			public void handle(ActionEvent event) {
+					try {
+							saveText(fc,primaryStage,textarea);
+					} catch (Exception e) {
+						// TODO 自動生成された catch ブロック
+						e.printStackTrace();
+					}
+				}
+		});
+//		pane.setCenter(new Label("edit"));
+		box .getChildren().add(fopen);
+		box.getChildren().add(fsave);
 		pane.setBottom(btn);
-		pane.setLeft(fopen);
+		pane.setLeft(box);
+		pane.setCenter(textarea);
 		Scene scene = new Scene(pane, 1366, 768);
 		primaryStage.setScene(scene);//Scene:表示する内容を組み込む
 	}
@@ -85,17 +106,24 @@ public class EditScene extends AbstractScene {
 	public void editImage(){
 
 	}
-	public void saveText(){
+	private static void saveText(FileChooser fc,Stage stage,TextArea area){
+		try {
+			FileWriter filewriter = new FileWriter(fc.showSaveDialog(stage));
+			filewriter.write(area.getText());
+			filewriter.close();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 	}
-	private static void fileOpen(FileChooser fc,Stage stage,BorderPane pane){
+
+	private static void fileOpen(FileChooser fc,Stage stage,BorderPane pane,TextArea area){
 		File openFile = fc.showOpenDialog(stage);
 		if(openFile != null){
 			try {
 				String text = new Scanner(openFile).useDelimiter("\\Z").next();
-				pane.setLeft(new Label(text));
-//				Writer writer = new FileWriter(fc.showSaveDialog(stage));
-//				writer.close();
+				area.setText(text);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
