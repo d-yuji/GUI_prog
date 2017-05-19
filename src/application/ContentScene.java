2fd6a23c28a39c43c45b528d69b6f7dc8765f72d
d.yuji <bronzsord@gmail.com>
2017-05-19 11:29:47 +0900

    画面遷移のテストとコントローラーの追加

--- src/application/ContentScene.java
+++ src/application/ContentScene.java
@@ -1,5 +1,8 @@
 package application;
 
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -12,7 +15,14 @@ public class ContentScene extends AbstractScene {
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-
+		BorderPane pane = new BorderPane();
+		System.out.println("test");
+		Scene scene = new Scene(pane, 1366, 768);
+		scene.getStylesheets().add("application/css/application.css");
+		primaryStage.getIcons().add(new Image("application/res/images/icon.png"));
+		primaryStage.setTitle("icontest");
+		primaryStage.setScene(scene);//Scene:表示する内容を組み込む
+		primaryStage.show();//ウィンドウの表示
 	}
 
 	@Override
--- src/application/Starter.java
+++ src/application/Starter.java
@@ -1,10 +1,5 @@
 package application;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -24,6 +19,7 @@ public class Starter extends Application {
 	public void start(Stage primaryStage) {
 			Button btn = new Button();
 			FileChooser fc = new FileChooser();
+			AbstractScene sceneContent = new ContentScene();
 			BorderPane pane = new BorderPane();
 			Image img = new Image("application/res/images/icon.png");
 			ImageView imgView = new ImageView();
@@ -39,7 +35,7 @@ public class Starter extends Application {
 			btn.setOnAction(new EventHandler<ActionEvent>() {
 					@Override
 					public void handle(ActionEvent event) {
-						File openFile = fc.showOpenDialog(primaryStage);
+						/*File openFile = fc.showOpenDialog(primaryStage);
 						if(openFile != null){
 							System.out.println("get file path");
 							try {
@@ -49,19 +45,29 @@ public class Starter extends Application {
 							} catch (IOException e) {
 								e.printStackTrace();
 							}
+						}*/
+
+						try {
+							sceneContent.start(primaryStage);
+						} catch (Exception e) {
+							// TODO 自動生成された catch ブロック
+							e.printStackTrace();
 						}
+
 					}
 				}
 			);
 
 			pane.setRight(imgView);
 			pane.setBottom(btn);
+
 			Scene scene = new Scene(pane, 1366, 768);
 			scene.getStylesheets().add("application/css/application.css");
 			primaryStage.getIcons().add(new Image("application/res/images/icon.png"));
 			primaryStage.setTitle("icontest");
 			primaryStage.setScene(scene);//Scene:表示する内容を組み込む
 			primaryStage.show();//ウィンドウの表示
+
 	}
 	public static void main(String[] args) {
 		launch(args);//GUIの起動
